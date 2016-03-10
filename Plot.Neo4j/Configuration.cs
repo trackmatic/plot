aadcee27be49bfa506b993b0bf2d3cb3987e491d
Ross Jones <rossj@trackmatic.co.za>
2016-03-10 14:22:32 +0200

    add configuration class

---
+++ Plot.Neo4j/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Neo4jClient;
+using Plot.Metadata;
+using Plot.Neo4j.Queries;
+using Plot.Proxies;
+
+namespace Plot.Neo4j
+{
+    public static class Configuration
+    {
+        public static IGraphSessionFactory CreateGraphSessionFactory(Uri uri, params Assembly[] mapperAssemblies)
+        {
+            var db = new GraphClient(uri);
+            db.Connect();
+            var entityStateFactory = new EntityStateCacheFactory();
+            var metadataFactory = new AttributeMetadataFactory();
+            var proxyFactory = new DynamicProxyFactory(metadataFactory);
+            var transactionFactory = new CypherTransactionFactory(db);
+            var repositoryFactory = new RepositoryFactory(db, transactionFactory, proxyFactory, metadataFactory, mapperAssemblies);
+            var queryExecutorFactory = new QueryExecutorFactory(db, mapperAssemblies);
+            var factory = new GraphSessionFactory(queryExecutorFactory, repositoryFactory, entityStateFactory);
+            return factory;
+        }
+    }
+}
--- Plot.Sample.Host/Program.cs
+++ Plot.Sample.Host/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using Neo4jClient;
-using Plot.Metadata;
 using Plot.Neo4j;
-using Plot.Neo4j.Queries;
-using Plot.Proxies;
 using Plot.Sample.Data.Mappers;
 using Plot.Sample.Queries;
 
@@ -13,18 +9,8 @@ namespace Plot.Sample.Host
     {
         private static void Main(string[] args)
         {
-            var db = new GraphClient(new Uri("http://neo4j:trackmatic@localhost:7474/db/data"));
-
-            db.Connect();
-
-            var entityStateFactory = new EntityStateCacheFactory();
-            var metadataFactory = new MetadataFactory();
-            var proxyFactory = new DynamicProxyFactory(metadataFactory);
-            var transactionFactory = new CypherTransactionFactory(db);
-            var repositoryFactory = new RepositoryFactory(db, transactionFactory, proxyFactory, metadataFactory, typeof (OrganisationMapper).Assembly);
-            var queryExecutorFactory = new QueryExecutorFactory(db, typeof (OrganisationMapper).Assembly);
-            var factory = new GraphSessionFactory(queryExecutorFactory, repositoryFactory, entityStateFactory);
-
+            var uri = new Uri("http://neo4j:trackmatic@localhost:7474/db/data");
+            var factory = Configuration.CreateGraphSessionFactory(uri, typeof(OrganisationMapper).Assembly);
             using (var session = factory.OpenSession())
             {
                 var results = session.Query(new GetOrganisationsByName()).Data;
